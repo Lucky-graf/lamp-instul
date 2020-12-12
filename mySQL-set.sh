87b1f07346fc84eb7081d441486a12cefabe6e71
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2020-12-13 01:42:01 +0200

    Update mySQL-set.sh

--- mySQL-set.sh
+++ mySQL-set.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo Settings Your acounte 'in' MySQL
 sudo mysql<<EOF
 SELECT user,authentication_string,plugin,host FROM mysql.user;
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'BOGdan16092001';
@@ -7,10 +7,10 @@ FLUSH PRIVILEGES;
 SELECT user,authentication_string,plugin,host FROM mysql.user;
 EOF
 mysql -u root -p root<<EOF
-CREATE USER 'graf'@'localhost' IDENTIFIED BY 'BOGdan16092001';
+CREATE USER 'graf'@'localhost' IDENTIFIED BY 'Graf1234';
 GRANT ALL PRIVILEGES ON *.* TO 'graf'@'localhost' WITH GRANT OPTION;
 EOF
 systemctl status mysql.service
-sudo mysqladmin -proot -u root version
+sudo mysqladmin -p root -u root version
 echo 'MySQL working'
-
+echo User: 'graf@localhost' \n Pasword: 'Graf1234'
