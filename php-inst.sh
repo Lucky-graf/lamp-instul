515329349b81b26a53c1b3a4d7fdbebfbaf6d2cd
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2020-12-13 01:45:03 +0200

    Update php-inst.sh

--- php-inst.sh
+++ php-inst.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+echo PHP instal and you can Chek info 'for' server https:\\IP\info,php
 sudo apt install php libapache2-mod-php
 sudo systemctl restart apache2
 sudo apt install php-fpm
 systemctl status php7.2-fpm
-echo '<?php phpinfo();'>/var/www/site1/info.php
+echo '<?php phpinfo();'>/var/www/site-car/info.php
 sudo systemctl restart apache2
 
