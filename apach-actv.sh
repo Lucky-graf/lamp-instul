0a390ffc3fc333366444489e7691e0311a817341
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2020-12-13 01:22:46 +0200

    Update apach-actv.sh

--- apach-actv.sh
+++ apach-actv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo a2ensite site1	# your_domain.conf
+sudo a2ensite site-car	# your_domain.conf
 sudo a2dissite 000-default.conf
 sudo apache2ctl configtest
 sudo systemctl restart apache2
