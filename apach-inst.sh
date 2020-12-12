de39747d1084f4312c49db65afb524a1ecedb51b
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2020-12-13 01:25:49 +0200

    Update apach-inst.sh

--- apach-inst.sh
+++ apach-inst.sh
@@ -6,5 +6,5 @@ sudo ufw app list
 sudo ufw allow 'Apache'
 sudo ufw status
 sudo systemctl status apache2
-sudo mkdir /var/www/site.got #your_domainsudo chown -R graf:graf /var/www/site.got #your_domain
-sudo chmod -R 755 /var/www/site.got #your_domain
+sudo mkdir /var/www/mysite  #your_domainsudo chown -R graf:graf /var/www/site.got #your_domain
+sudo chmod -R 755 /var/www/mysite  #your_domain
