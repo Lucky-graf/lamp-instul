598c1aeba72c2d51db2b69bcc77bb95d470a9b63
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2020-12-13 05:30:46 +0200

    Update php-inst.sh

--- php-inst.sh
+++ php-inst.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo PHP install and you can Chek info 'for' server https:\\IP\info ,php after deploy
-sleep 5s
+sleep 5
 sudo apt install php libapache2-mod-php
 sudo systemctl restart apache2
 sudo apt install php-fpm
