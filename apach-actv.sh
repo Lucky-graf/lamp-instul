c4d748dc961689f140f4d70f5bbbb030a49685d7
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2020-12-13 08:29:26 +0200

    Update apach-actv.sh

--- apach-actv.sh
+++ apach-actv.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
+sudo mv /home/graf/deploy/lamp-instul/site.conf /etc/apache2/sites-available/site-car.conf 	#копіювати вміст 000df
 echo Settings Apache
 sleep 3
 sudo a2ensite site-car	# your_domain.conf
-sudo a2dissite 000-default.conf
+sudo a2dissite site-car.conf
 sudo apache2ctl configtest
 sudo systemctl restart apache2
 echo Edit config 'for' YOU
 pwd
-sudo mv /home/graf/deploy/lamp-instul/site.conf /etc/apache2/sites-available/site-car.conf 	#копіювати вміст 000df
+
 sleep 10
 echo Settings Apache successful!
