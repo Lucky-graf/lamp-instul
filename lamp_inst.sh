99ccf5d53d7ac83d999030abd1d7a492e246fab5
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2020-12-13 06:36:57 +0200

    Update lamp_inst.sh

--- lamp_inst.sh
+++ lamp_inst.sh
@@ -1,10 +1,4 @@
 #!bin/bash/
-pwd
-sleep 3
-time=$(date +%s)
-source ./progress/progress_bar.sh
-
-generate_some_output_and_sleep() {
     sudo bash apach-inst.sh
     sudo bash apach-actv.sh
     sudo bash file-serv.sh
@@ -12,23 +6,3 @@ generate_some_output_and_sleep() {
     sudo bash mySQL-set.sh
     sudo bash php-inst.sh
     sudo bash certbot.sh
-
-}
-
-
-main() {
-    # Make sure that the progress bar is cleaned up when user presses ctrl+c
-    enable_trapping
-    # Create progress bar
-    setup_scroll_area
-    for i in {1..99}
-    do
-    sleep 0.1
-    generate_some_output_and_sleep
-    draw_progress_bar $i
-    done
-    destroy_scroll_area
-    echo 'This operation took-' $(($(date +%s)-$time)) '-time!'
-}
-
-
