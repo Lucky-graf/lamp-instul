be95c0a440127902a82e1b24e8ee65f5f086ac0f
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2020-12-13 05:53:02 +0200

    Update lamp_inst.sh

--- lamp_inst.sh
+++ lamp_inst.sh
@@ -1,28 +1,26 @@
 #!bin/bash/
 pwd
 time=$(date +%s)
-#*source /home/graf/deploy/lamp-instul/progress/progress_bar.sh
+source ./progress/progress_bar.sh
 
-#main() {
+main() {
     # Make sure that the progress bar is cleaned up when user presses ctrl+c
-    #enable_trapping
+    enable_trapping
     # Create progress bar
-    #setup_scroll_area
-    #for i in {1..99}
-    #    draw_progress_bar $i
-   #     sleep 5
-  #  done
- #       destroy_scroll_area
-#}
+    setup_scroll_area
+    for i in {1..99}
+        draw_progress_bar $i
+        sleep 5
+    done
+        destroy_scroll_area
+}
 
-pwd
 sudo bash apach-inst.sh
-pwd
 sudo bash apach-actv.sh
 sudo bash file-serv.sh
 sudo bash mySQL-inst.sh
 sudo bash mySQL-set.sh
 sudo bash php-inst.sh
 sudo bash certbot.sh
-#destroy_scroll_area
+destroy_scroll_area
 echo 'This operation took-' $(($(date +%s)-$time)) '-time!'
