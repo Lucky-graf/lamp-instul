ca89974343c2fa0dc7d78b9dd3a8e60424c238b5
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2020-12-13 08:14:15 +0200

    Update lamp_inst.sh

--- lamp_inst.sh
+++ lamp_inst.sh
@@ -5,6 +5,6 @@
     sudo bash mySQL-inst.sh
     sudo bash mySQL-set.sh
     sudo bash php-inst.sh
-    echo Start BOT
-    sudo bash cerbot.sh
-sleep 7
+    #echo Start BOT
+    #sudo bash cerbot.sh
+#sleep 7
